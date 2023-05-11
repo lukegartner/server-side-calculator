86e87329c7e849606b20ecbaf5ab83ddf89450a2
Luke Gartner <lukegartner4@gmail.com>
2023-05-11 12:38:46 -0500

    setup DOM variables in client.js and created handleCalculation function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,10 @@
-console.log("hello from client.js");
+// DOM variables
+const num1Input = document.querySelector("#num1");
+const num2Input = document.querySelector("#num2");
+const operationBtns = document.querySelectorAll(".operation-btn");
+const clearBtn = document.querySelector("#clear");
+
+// Handle Calculation
+const handleCalculation = (e) => {
+  e.preventDefault();
+};
