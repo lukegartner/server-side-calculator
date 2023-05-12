d5d0a0b5800f0a3cc69c1c024ada7c54b2942fb1
Luke Gartner <lukegartner4@gmail.com>
2023-05-12 16:03:33 -0500

    modify evaluateExpression function to handle array with multiple arithmetic operations in correct order of operations

--- server/modules/evaluateExpression.js
+++ server/modules/evaluateExpression.js
@@ -1,6 +1,52 @@
 // Evaluate Math Expression
 
-const evaluateExpression = (num1, num2, operation) => {
+const evaluateExpression = (expression) => {
+  const expressionToEvaluate = expression.slice();
+  while (expressionToEvaluate.length > 1) {
+    // Two loops to separate order of operations
+    // Multiplication and Division
+    while (
+      expressionToEvaluate.includes("*") ||
+      expressionToEvaluate.includes("/")
+    ) {
+      for (let i = 0; i < expressionToEvaluate.length; i++) {
+        if (expressionToEvaluate[i] === "*") {
+          let result =
+            Number(expressionToEvaluate[i - 1]) *
+            Number(expressionToEvaluate[i + 1]);
+          expressionToEvaluate.splice(i - 1, 3, result);
+          console.log("exp: /", expressionToEvaluate);
+        }
+        if (expressionToEvaluate[i] === "/") {
+          let result =
+            Number(expressionToEvaluate[i - 1]) /
+            Number(expressionToEvaluate[i + 1]);
+          expressionToEvaluate.splice(i - 1, 3, result);
+          console.log("exp: *", expressionToEvaluate);
+        }
+      }
+    }
+    // Addition and Subtraction
+    for (let i = 0; i < expressionToEvaluate.length; i++) {
+      if (expressionToEvaluate[i] === "+") {
+        let result =
+          Number(expressionToEvaluate[i - 1]) +
+          Number(expressionToEvaluate[i + 1]);
+        expressionToEvaluate.splice(i - 1, 3, result);
+        console.log("exp: +", expressionToEvaluate);
+      }
+      if (expressionToEvaluate[i] === "-") {
+        let result =
+          Number(expressionToEvaluate[i - 1]) -
+          Number(expressionToEvaluate[i + 1]);
+        expressionToEvaluate.splice(i - 1, 3, result);
+        console.log("exp: -", expressionToEvaluate);
+      }
+    }
+  }
+
+  return expressionToEvaluate[0];
+
   switch (operation) {
     case "+":
       return num1 + num2;
